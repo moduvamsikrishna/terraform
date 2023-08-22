dd92b6c4040c858c9859b9d17a9a43a5d3e90910
moduvamsikrishna <139459943+moduvamsikrishna@users.noreply.github.com>
2023-08-22 18:49:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,13 @@ resource "local_file" "pet" {
 filename = "pets.txt"
 content = "we love pets!"
 }
+variable "filename" { 
+}
+variable "content" {
+  }
+variable "prefix" {
+}
+variable "separator" {
+}
+variable "length" {
+}
