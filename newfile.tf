baeb85b86d2d84df4f75ea0238d1cb56afdab483
moduvamsikrishna <139459943+moduvamsikrishna@users.noreply.github.com>
2023-08-22 18:56:52 +0530

    Update newfile.tf

--- newfile.tf
+++ newfile.tf
@@ -1,10 +1,15 @@
-variable "filename" { 
+variable "filename" {
+default = "new file.txt"
 }
 variable "content" {
+default = "vamsi krishna"
   }
 variable "prefix" {
+default = "mr"
 }
 variable "separator" {
+default = ":"
 }
 variable "length" {
+default = "1"
 }
